f50063c25b4fe82e5bf965b248116ad24f452f28
Josh Akeman <joshakeman@gmail.com>
2019-02-26 17:52:30 -0500

    callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -64,7 +64,15 @@ multiplyNums(4, 3, function(multiplied) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(item, list);
 }
+
+contains('Gum', items, function(a, b) {
+  const lookUp = b.filter( function(currentValue){
+    console.log( currentValue == a);
+  });
+});
+
 
 /* STRETCH PROBLEM */
 
